ff316202d97d4752523311b881d8b2f91849b59c
Daniel Bonifacio <contato@danielbonifacio.com.br>
2019-08-19 08:38:54 -0300

    fix(architecture): remove smallfiles flag from docker-compose + environment file suffix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
+import path from 'path'
 import dotenv from 'dotenv'
-dotenv.config()
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV.toLocaleLowerCase()}`)
+})
 
 export default {
   app: {
